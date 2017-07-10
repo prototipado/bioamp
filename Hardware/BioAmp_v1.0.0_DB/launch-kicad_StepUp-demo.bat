594b06db3edec17d60acc6a812e27d2f727f4756
jmreta <jmreta@bioingenieria.edu.ar>
2017-07-10 12:14:20 -0300

    Update Hardware/Bioamp_v1.0.0_DB: Project Folder Tree

--- Hardware/BioAmp_v1.0.0_DB/launch-kicad_StepUp-demo.bat
+++
@@ -1,6 +0,0 @@
-cd %~p0
-
-::start "" "%PROGRAMFILES%\FreeCAD 0.15\bin\freecad" BioAmp_v1.0.0_DB.emn kicad_StepUp.FCMacro
-
-:: freecad <IDF file> <configuration file> kicad_StepUp.FCMacro
-start "" "%PROGRAMFILES%\FreeCAD 0.15\bin\freecad" BioAmp_v1.0.0_DB.emn ksu-config.cfg kicad_StepUp.FCMacro
